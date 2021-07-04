f70d9f5c52a105567f75e788638edb724574bb74
App Generator <support@appseed.us>
2021-07-04 16:15:04 +0300

    Apply Joy validation schema
    - login
    - register

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 */
 const express = require('express');
+const Joi = require('joi');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const bcrypt = require('bcrypt-nodejs');
@@ -15,7 +16,109 @@ const ActiveSession = require('../models/activeSession');
 const reqAuth = require('../config/safeRoutes').reqAuth;
 const { smtpConf } = require('../config/config');
 
-// route /admin/users/
+// Route: <HOST>:PORT/api/users/
+
+const userSchema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    username: Joi.string().alphanum().min(4).max(15).optional(),
+    password: Joi.string().required()
+});
+
+router.post('/register', (req, res) => {
+
+    // Joy Validation
+    const result = userSchema.validate(req.body);
+    if (result.error) {
+        res.status(422).json({ success: false, 
+                               msg: 'Validation err: ' + result.error.details[0].message });
+        return;
+    }
+
+    const { username, email, password } = req.body;
+
+    User.findOne({ email: email }).then((user) => {
+
+        if (user) {
+
+            res.json({ success: false, msg: 'Email already exists' });
+
+        } else {
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, null, (err, hash) => {
+                    if (err) throw err;
+                    const query = {
+                        username: username,
+                        email: email,
+                        password: hash
+                    };
+                    User.create(query, function(err, user) {
+                        if (err) throw err;
+
+                        // eslint-disable-next-line max-len
+                        res.json({ success: true, userID: user._id, msg: 'The user was succesfully registered' });
+                    });
+                });
+            });
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+
+    // Joy Validation
+    const result = userSchema.validate(req.body);
+    if (result.error) {
+        res.status(422).json({ success: false, 
+                               msg: 'Validation err: ' + result.error.details[0].message });
+        return;
+    }
+       
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email }, (err, user) => {
+        if (err) throw err;
+
+        if (!user) {
+            return res.json({ success: false, msg: 'Wrong credentials' });
+        }
+
+        bcrypt.compare(password, user.password, function(err, isMatch) {
+            if (isMatch) {
+                const token = jwt.sign(user, config.secret, {
+                    expiresIn: 86400, // 1 week
+                });
+                // Don't include the password in the returned user object
+                const query = { userId: user._id, token: token };
+                ActiveSession.create(query, function(err, cd) {
+                    user.password = null;
+                    user.__v = null;
+                    return res.json({
+                        success: true,
+                        token: token,
+                        user,
+                    });
+                });
+            } else {
+                return res.json({ success: false, msg: 'Wrong credentials' });
+            }
+        });
+    });
+});
+
+router.post('/logout', reqAuth, function(req, res) {
+    const token = req.body.token;
+    ActiveSession.deleteMany({ token: token }, function(err, item) {
+        if (err) {
+            res.json({ success: false });
+        }
+        res.json({ success: true });
+    });
+}); 
+
+router.post('/checkSession', reqAuth, function(req, res) {
+    res.json({ success: true });
+});
 
 router.post('/all', reqAuth, function(req, res) {
     User.find({}, function(err, users) {
@@ -51,83 +154,5 @@ router.post('/edit', reqAuth, function(req, res) {
         }
     });
 });
-
-router.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-
-    User.findOne({ email: email }).then((user) => {
-        if (user) {
-            res.json({ success: false, msg: 'Email already exists' });
-            //} else if (password.length < 6) {
-            //  // eslint-disable-next-line max-len
-            //  res.json({success: false, msg: 'Password must be at least 6 characters long'});
-        } else {
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(password, salt, null, (err, hash) => {
-                    if (err) throw err;
-                    const query = {
-                        username: username,
-                        email: email,
-                        password: hash
-                    };
-                    User.create(query, function(err, user) {
-                        if (err) throw err;
-
-                        // eslint-disable-next-line max-len
-                        res.json({ success: true, userID: user._id, msg: 'The user was succesfully registered' });
-                    });
-                });
-            });
-        }
-    });
-});
-
-router.post('/login', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({ email: email }, (err, user) => {
-        if (err) throw err;
-
-        if (!user) {
-            return res.json({ success: false, msg: 'Wrong credentials' });
-        }
-
-        bcrypt.compare(password, user.password, function(err, isMatch) {
-            if (isMatch) {
-                const token = jwt.sign(user, config.secret, {
-                    expiresIn: 86400, // 1 week
-                });
-                // Don't include the password in the returned user object
-                const query = { userId: user._id, token: token };
-                ActiveSession.create(query, function(err, cd) {
-                    user.password = null;
-                    user.__v = null;
-                    return res.json({
-                        success: true,
-                        token: token,
-                        user,
-                    });
-                });
-            } else {
-                return res.json({ success: false, msg: 'Wrong credentials' });
-            }
-        });
-    });
-});
-
-router.post('/checkSession', reqAuth, function(req, res) {
-    res.json({ success: true });
-});
-
-router.post('/logout', reqAuth, function(req, res) {
-    const token = req.body.token;
-    ActiveSession.deleteMany({ token: token }, function(err, item) {
-        if (err) {
-            res.json({ success: false });
-        }
-        res.json({ success: true });
-    });
-}); 
 
 module.exports = router;

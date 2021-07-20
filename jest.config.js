eaa01de2c07afb0cba0a403d990996102716eff2
App Generator <support@appseed.us>
2021-07-20 17:13:37 +0300

    CleanUp for TypeORM / SQLite Migration

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  setupFiles: ['dotenv/config'],
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['node_modules'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
-};
--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-/*
-
-Copyright (c) 2019 - present AppSeed.us
-
-*/
-export default {
-  webURL: 'https://localhost:5000/',
-};
